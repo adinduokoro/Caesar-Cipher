cca3910fc1f5840eb4a7690079757f75f3df6c3e
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-06-30 17:53:00 -0400

    Update Caesar Cipher.py
    Updated the converter function.

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,22 +1,27 @@
-def converter():
+def converter():  # This function will either use a encrypt or decrypt formula
+    new_position = ""
     position = alphabet.find(character)
-    new_position = (position + key) % 26
+    if response == "E":   # Encrypt formula
+        new_position = (position + key) % 26
+    elif response == "D":  # Decrypt formula
+        new_position = (position - key) % 26
     new_character = alphabet[new_position]
     return new_character
 
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-while True:
-    response = input("Do you want to Encrypt or Decrypt: ").upper()
+while True:  # Ask the user if they want to encrypt or decrypt
+    response = input("Do you want to (E)ncrypt or (D)ecrypt: ").upper()
     if response == "E" or response == "D":
         break
 
-user_message = input("Enter your secret message: ")  # Get the message from the user
-key = int(input("What is the key: "))  # Get the key from the user
+# Ask the user for the message or the key
+user_message = input("Enter your secret message: ")
+key = int(input("What is the key: "))
 
 secret_message = ""
-for character in user_message:
+for character in user_message:  # Handles uppercase, lowercase, and space characters
     if character.isupper():
         character = character.lower()
         if character in alphabet:
@@ -28,4 +33,4 @@ for character in user_message:
             secret_message += converter()
     else:
         secret_message += character
-print(secret_message)
+print(">" + secret_message)
