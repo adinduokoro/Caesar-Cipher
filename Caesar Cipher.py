bddf847be909fa112ad5afa6e2859f5715ef009e
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-06-30 16:52:07 -0400

    Update Caesar Cipher.py
    Checks if the message contains an upper case or space. Also, a converter was defined to store the ceaser cipher formula.

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,13 +1,26 @@
+def converter(alphabet):
+    position = alphabet.find(character)
+    new_position = (position + key) % 26
+    new_character = alphabet[new_position]
+    return new_character
+
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 user_message = input("Enter your secret message: ")  # Get the message from the user
 key = int(input("What is the key: "))  # Get the key from the user
 
-secret_message = ""  # Holds the secret message
-for character in user_message:  # Updates the character in the user message
-    if character in alphabet:
-        position = alphabet.find(character)
-        new_position = (position + key) % 26
-        new_character = alphabet[new_position]
-        secret_message += new_character
+secret_message = ""
+for character in user_message:
+    if character.isupper():
+        character = character.lower()
+        if character in alphabet:
+            converter(alphabet)
+            secret_message += converter(alphabet).upper()
+    elif character.islower():
+        if character in alphabet:
+            converter(alphabet)
+            secret_message += converter(alphabet)
+    else:
+        secret_message += character
 print(secret_message)
